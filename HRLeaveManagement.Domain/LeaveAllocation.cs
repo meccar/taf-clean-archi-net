45efd7bcd6a93600c254ff98c2628456aa0b8764
meccar <tung14hn@gmail.com>
2024-08-28 23:42:08 +0800

    feat (HrLeave): implemented core

---
+++ HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Domain.common;
+
+namespace HR.LeaveManagement.Domain;
+public class LeaveAllocation : BaseEntity
+{
+    public int Id { get; set; }
+    public int NumberOfDays { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+}
