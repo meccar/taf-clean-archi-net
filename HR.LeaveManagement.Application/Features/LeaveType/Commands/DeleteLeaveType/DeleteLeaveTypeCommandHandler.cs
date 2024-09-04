f8afb1d723f785aa370e5acf56edb06e1d7d6fab
meccar <tung14hn@gmail.com>
2024-09-04 22:08:53 +0800

    feat (LeaveType): implemented update & delete

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+public class DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+    : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository = leaveTypeRepository;
+
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        var leaveTypeToDelete = await _leaveTypeRepository.GetLeaveByIdAsync(request.Id);
+
+        await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+        return Unit.Value;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+public class UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository) 
+    : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+{
+    private readonly IMapper _mapper = mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository = leaveTypeRepository;
+
+    public async Task<Unit> Handle(UpdateLeaveTypeCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+        await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+        return Unit.Value;
+    }
+}
